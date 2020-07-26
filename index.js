38eb94bb597354cabd1d86139b3eb06f4ab00928
Constantine Huzenko <kossguzenko@gmail.com>
2020-07-26 15:16:26 +0300

    remove console log of btn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let contactBlock = document.querySelector(".contact__inner");
 let iconOpen = document.querySelector(".contact__btn-open");
 let iconeClose = document.querySelector(".contact__btn-close");
 
-console.log(contactBtn);
-
 contactBtn.addEventListener("click", (e) => {
   e.preventDefault();
   contactBlock.classList.toggle("open");
